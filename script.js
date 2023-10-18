b28c6ee1897b7229bd7571139903400d21d68513
mi <mirellanaspolini35@gmail.com>
2023-10-18 12:29:57 -0300

    WIP: formula pra contar novelos

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-var valorTotalHoras = 0;
-var quantAgulha = 1;
-var precoAgulha = 3.65;
-var valorTotal = 0;
-var pecaFinalAltura = 0;
-var pecaFinalLargura = 0;
-var areaPecaFinal = 0
-var ConsumoTotalFio = 0;
-var fioescolhido = document.getElementById("selectLinha").value
-var valorNovelo = 0;
-var numeroNovelosNecessario = 0;
-var valorTotalNovelo = 0;
-var quantEnchimento = 0
-var ehChaveiro = false;
+let valorTotalHoras = 0;
+let valorTotal = 0;
+
+// informações sobre agulha
+let quantAgulha = 0;
+const precoAgulha = 3.65;
+
+// informações sobre a peça final
+let pecaFinalAltura = 0;
+let pecaFinalLargura = 0;
+let areaPecaFinal = 0;
+
+// informações sobre o fio
+let ConsumoTotalFio = 0;
+let fioescolhido = document.getElementById("selectLinha").value;
+let valorNovelo = 0;
+let numeroNovelosNecessario = 0;
+let valorTotalNovelo = 0;
+
+let quantEnchimento = 0;
+let valorAdicional = 0;
 
 // Função para calcular o valor das horas
 function calculaHora() {
@@ -25,87 +32,97 @@ function calculaQuantAgulha() {
     atualizarTotal();
 }
 
-// Função para atualizar o total e aplicar o lucro
-function atualizarTotal() {
-    var precoChaveiro = ehChaveiro ? 1.00 : 0 
-    var valores = quantAgulha + valorTotalHoras + valorTotalNovelo + 0.60 + 0.90 + quantEnchimento + precoChaveiro;
-    var totalComLucro = valores * 0.2 + valores;
-    document.querySelector("#total").textContent = "Total: R$ " + totalComLucro.toFixed(2); // Formate para duas casas decimais
+function calculaValorAdicional({ target }) {
+    valorAdicional = target.checked
+        ? valorAdicional + Number(target.dataset.preco)
+        : valorAdicional - Number(target.dataset.preco);
 }
 
-function guardaLargura({target}) {
-    console.log(target);
+// Função para atualizar o total e aplicar o lucro
+function atualizarTotal() {
+    let valores =
+        quantAgulha +
+        valorTotalHoras +
+        valorTotalNovelo +
+        quantEnchimento +
+        valorAdicional;
+    let totalComLucro = valores * 0.2 + valores;
+    document.querySelector("#total").textContent =
+        "Total: R$ " + totalComLucro.toFixed(2); // Formate para duas casas decimais
 }
 
 // Adicione event listeners para acompanhar as alterações nos elementos de entrada
 document.getElementById("quantHoras").addEventListener("input", calculaHora);
-document.getElementById("quantAgulhas").addEventListener("input", calculaQuantAgulha);
-document.getElementById("alturaFinal").addEventListener("input", ({target}) => pecaFinalAltura = target.value)
-document.getElementById("larguraFinal").addEventListener("input", ({target}) => pecaFinalLargura = target.value)
-document.getElementById("quantEnchimento").addEventListener("input", ({target}) => {
-    quantEnchimento = target.value / 10;
-    atualizarTotal();
-})
-document.getElementById("chaveiro").addEventListener("change", () => { ehChaveiro = !ehChaveiro; atualizarTotal()});
+document
+    .getElementById("quantAgulhas")
+    .addEventListener("input", calculaQuantAgulha);
+document
+    .getElementById("alturaFinal")
+    .addEventListener(
+        "input",
+        ({ target }) => (pecaFinalAltura = target.value)
+    );
+document
+    .getElementById("larguraFinal")
+    .addEventListener(
+        "input",
+        ({ target }) => (pecaFinalLargura = target.value)
+    );
+document
+    .getElementById("quantEnchimento")
+    .addEventListener("input", ({ target }) => {
+        quantEnchimento = target.value / 10;
+        atualizarTotal();
+    });
 
+const listaCheckbox = document.querySelectorAll('input[type="checkbox"]');
 
-
-
-
-
+for (let i = 0; i < listaCheckbox.length; i++) {
+    listaCheckbox[i].addEventListener("input", calculaValorAdicional);
+}
 
 // -~-~-~-~-~-~-~-~--~-~-~-~-~--~~-~-
 
 console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-
 function teste() {
-    fioescolhido = document.getElementById("selectLinha").value
-
     // amostra
     let amostraFioLargura = 0;
     let amostraFioAltura = 0;
-    let amostraPeso = 0
-    let pesoNovelo = 50
+    let amostraPeso = 0;
+    let pesoNovelo = 50;
 
-    if (fioescolhido === 'balloonAmigo'){
+    if (fioescolhido === "balloonAmigo") {
         amostraFioLargura = 10;
         amostraFioAltura = 5;
         amostraPeso = 4.5;
         pesoNovelo = 50;
         valorNovelo = 7.25;
-    } else if (fioescolhido === 'amigurumiPelucia'){
+    } else if (fioescolhido === "amigurumiPelucia") {
         amostraFioLargura = 10;
         amostraFioAltura = 5;
         amostraPeso = 4;
         pesoNovelo = 85;
-        valorNovelo = 10.40;
+        valorNovelo = 10.4;
     }
 
-    // areaAmostra = A * L
     let areaAmostra = amostraFioAltura * amostraFioLargura;
 
-    console.log("area da amostra do fio:", areaAmostra, "cm²");
-
-    
-
-    // let areaPecaFinal = Math.PI * ((pecaFinalLargura / 2) ** 2)
-    areaPecaFinal = pecaFinalAltura * pecaFinalLargura
-    console.log("area da peça final:", areaPecaFinal, "cm²");
-
+    areaPecaFinal = pecaFinalAltura * pecaFinalLargura;
     ConsumoTotalFio = Math.ceil((amostraPeso / areaAmostra) * areaPecaFinal);
+    numeroNovelosNecessario = ConsumoTotalFio / pesoNovelo;
+    valorTotalNovelo = numeroNovelosNecessario * valorNovelo;
+    atualizarTotal();
+
+    console.log("area da amostra do fio:", areaAmostra, "cm²");
+    console.log("area da peça final:", areaPecaFinal, "cm²");
     console.log("consumo total:", ConsumoTotalFio);
-
-
-    numeroNovelosNecessario = ConsumoTotalFio / pesoNovelo
     console.log("novelo no total:", numeroNovelosNecessario);
-
-    valorTotalNovelo = numeroNovelosNecessario * valorNovelo
     console.log("valor novelo: ", valorTotalNovelo);
+    console.log("enchimento", quantEnchimento);
 
-
-console.log("enchimento", quantEnchimento);
- 
-    atualizarTotal()
-    document.getElementById("resultado").textContent = "Você irá precisar de " + Math.ceil(numeroNovelosNecessario) +" novelos!"
+    document.getElementById("resultado").textContent =
+        "Você irá precisar de " +
+        Math.ceil(numeroNovelosNecessario) +
+        " novelos!";
 }
